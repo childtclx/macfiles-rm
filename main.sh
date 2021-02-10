dd2c3eae784379b56c3f5383e13de197c7c4f494
dparticle <dparticle@163.com>
2021-02-10 20:18:43 +0800

    refactor main.sh

--- main.sh
+++ main.sh
@@ -1,39 +1,51 @@
 #! /bin/bash
 
 function mac_files_del() {
-    # because of hidden file included "." and "..", they are bad variable name
-    filelist=`ls -a $1`
-    for file in $filelist
+    ls -a "$1" \
+    | while read file;
     do
+        # because of hidden file included "." and "..", they are bad variable name
         if [[ "$file" != "." &&  "$file" != ".." ]];
         then
-            if [ -d $1/$file ];
+            if [[ -d "$1/$file" ]];
             then
-                mac_files_del $1/$file
-            elif [[ $file =~ ".DS_Store" ]] || [[ $file = "Thumbs.db" ]];
+                if [[ "$file" = "__MACOSX" ]];
+                then
+                    rm -rf "$1/$file"
+                    echo "$1/$file"
+                else
+                    mac_files_del "$1/$file"
+                fi
+            elif [[ "$file" = "._*" ]] || [[ "$file" =~ ".DS_Store" ]] || [[ "$file" = "Thumbs.db" ]];
             then
-                rm -rf $1/$file
+                rm -rf "$1/$file"
                 # print path that is deleted
-                echo $1/$file
+                echo "$1/$file"
             fi
         fi
     done
 }
 
 function mac_files_move() {
-    filelist=`ls -a $1`
-    for file in $filelist
+    ls -a "$1" \
+    | while read file;
     do
         if [[ "$file" != "." &&  "$file" != ".." ]];
         then
-            if [ -d $1/$file ];
+            if [[ -d "$1/$file" ]];
             then
-                mac_files_del $1/$file $2
-            elif [[ $file =~ ".DS_Store" ]] || [[ $file = "Thumbs.db" ]];
+                if [[ "$file" = "__MACOSX" ]];
+                then
+                    mkdir -p "$move_path${1#*$dname}"
+                    mv "$1/$file" "$move_path${1#*$dname}"
+                else
+                    mac_files_move "$1/$file"
+                fi
+            elif [[ "$file" = "._*" ]] || [[ "$file" =~ ".DS_Store" ]] || [[ "$file" = "Thumbs.db" ]];
             then
-                move_path_add=${1#*$dname}
-                mkdir -p $2/$move_path_add
-                mv $1/$file $2/$move_path_add
+                # first to mkdir dir
+                mkdir -p "$move_path${1#*$dname}"
+                mv "$1/$file" "$move_path${1#*$dname}"
             fi
         fi
     done
@@ -41,14 +53,15 @@ function mac_files_move() {
 
 echo "Please enter a folder to delete:"
 read name
-name=${name#*\'}
-name=${name%\'*}
+# remove the start and end char("'") of path by regex
+name="${name#*\'}"
+name="${name%\'*}"
 # if delete, please commit the following 3 lines
 # dname=$(dirname "$name")
-# mkdir -p $dname/mac_files_recover
-# move_path=$dname/mac_files_recover
+# mkdir -p "$dname/mac_files_recover"
+# move_path="$dname/mac_files_recover"
 
 echo "------ Begin Handling Mac files ------"
-# mac_files_move $name $move_path
-mac_files_del $name
+# mac_files_move "$name"
+mac_files_del "$name"
 echo "-------------- In the end --------------"
